c35808d1bd268b7574f146d77de4451a308e2e11
Jim Barry <jbarry@pelicom.net>
2019-05-14 12:05:08 -0500

    First Commmit

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by James Barry on 5/9/19.
+//  Copyright © 2019 Jim Barry. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    // MARK: For purpose of making buttons so they are nor distorted
+    
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var chipmonkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        stopAudio()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+
+        slowButton.contentMode = .center
+        slowButton.imageView?.contentMode = .scaleAspectFit
+        fastButton.contentMode = .center
+        fastButton.imageView?.contentMode = .scaleAspectFit
+        chipmonkButton.contentMode = .center
+        chipmonkButton.imageView?.contentMode = .scaleAspectFit
+        darthVaderButton.contentMode = .center
+        darthVaderButton.imageView?.contentMode = .scaleAspectFit
+        echoButton.contentMode = .center
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        reverbButton.contentMode = .center
+        reverbButton.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+}
